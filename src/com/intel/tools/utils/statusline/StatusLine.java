ba32b3fc3df97903cf9e7b6cd50355654896f454
bgoudy <benoit.goudy@intel.com>
2014-11-20 09:41:18 +0100

    Added StatusLine static method to write messages to the status line of the workbench.

---
+++ src/com/intel/tools/utils/statusline/StatusLine.java
@@ -0,0 +1,35 @@
+package com.intel.tools.utils.statusline;
+
+import org.eclipse.jface.action.StatusLineManager;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.WorkbenchWindow;
+
+/**
+ * This class presnets statics methods to access the StatusLine from the workbench.
+ * 
+ *  !! This class uses internal methods from the Workbench !!
+ */
+public class StatusLine {
+
+    private static StatusLineManager manager;
+
+    public static void showMessage(String message) {
+        if (getStatusLineManager() != null)
+        {
+            getStatusLineManager().setMessage(message);
+        }
+    }
+
+    public static StatusLineManager getStatusLineManager()
+    {
+        if (manager == null) {
+            IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+            if (workbenchWindow != null) {
+                manager = ((WorkbenchWindow) workbenchWindow).getStatusLineManager();
+            } else
+                return null;
+        }
+        return manager;
+    }
+}
