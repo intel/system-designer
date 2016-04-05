9157ed1bda80ea9fb00578088c262a62be683e61
Romain Larvet <romainx.larvet@intel.com>
2016-04-05 13:47:01 +0200

    Topology Zoom : Use toggle button for fit to screen
    - We must have one fit to screen controller for each part to be able to toggle them individually
    - Deactivate auto fit to screen when zooming manually
    
    Change-Id: I7e56aedffccdf8ce7bf73172f6a463e182dd5dc7
    Tracked-On: RTC-175242
    Signed-off-by: Romain Larvet <romainx.larvet@intel.com>

--- src/com/intel/tools/fdk/graphframework/displayer/controller/FitToScreenController.java
+++ src/com/intel/tools/fdk/graphframework/displayer/controller/FitToScreenController.java
@@ -22,14 +22,47 @@
  */
 package com.intel.tools.fdk.graphframework.displayer.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 
 import com.intel.tools.fdk.graphframework.displayer.GraphDisplayer;
 import com.intel.tools.fdk.graphframework.figure.node.NodeBodyFigure;
 
-public abstract class FitToScreenController {
+public class FitToScreenController {
+
+    public interface Listener {
+        void fitToScreenChanged(boolean enabled);
+    }
+
+    private final List<Listener> listeners = new ArrayList<>();
+
+    private static final int KEY_0 = 224;
+
+    private boolean fitEnabled;
+
+    private final GraphDisplayer displayer;
+
+    public FitToScreenController(final GraphDisplayer displayer) {
+        this.displayer = displayer;
+        displayer.getControl().addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(final KeyEvent e) {
+                if ((e.stateMask & SWT.CTRL) == SWT.CTRL) {
+                    if (e.keyCode == KEY_0 || e.keyCode == SWT.KEYPAD_0) {
+                        // Ctrl + 0
+                        fitToScreen();
+                    }
+                }
+            }
+        });
+    }
 
     /**
      * Update the scale and figure position of the content layer displayer to feet the current screen
@@ -37,8 +70,8 @@ public abstract class FitToScreenController {
      * @param displayer
      *            the displayer to work on
      */
-    public static void fitToScreen(final GraphDisplayer displayer) {
-        fitToScreen(displayer, Integer.MAX_VALUE);
+    public void fitToScreen() {
+        fitToScreen(Integer.MAX_VALUE);
     }
 
     /**
@@ -49,12 +82,23 @@ public abstract class FitToScreenController {
      * @param scaleLimit
      *            maximum scale ratio to set to the scalable layer of the modified displayer
      */
-    public static void fitToScreen(final GraphDisplayer displayer, final int scaleLimit) {
+    public void fitToScreen(final int scaleLimit) {
         setupScale(displayer, scaleLimit);
         centerContent(displayer);
     }
 
-    private static void setupScale(final GraphDisplayer displayer, final int limit) {
+    public void setFitEnabled(final boolean enabled) {
+        this.fitEnabled = enabled;
+        for (final Listener listener : listeners) {
+            listener.fitToScreenChanged(enabled);
+        }
+    }
+
+    public boolean isFitEnabled() {
+        return fitEnabled;
+    }
+
+    private void setupScale(final GraphDisplayer displayer, final int limit) {
         double scale;
         final Dimension dimension = displayer.getContentLayer().getFreeformExtent().getCopy().getSize();
 
@@ -66,7 +110,7 @@ public abstract class FitToScreenController {
         }
     }
 
-    private static void centerContent(final GraphDisplayer displayer) {
+    private void centerContent(final GraphDisplayer displayer) {
         final Dimension viewDimension = displayer.getControl().getViewport().getSize();
 
         // Retrieve topology information
@@ -76,9 +120,9 @@ public abstract class FitToScreenController {
         final Dimension topologyDimension = topologyBounds.getSize();
         final Point topologyCoordinates = topologyBounds.getLocation();
         // Retrieve the delta between desired topology position and current one
-        final Dimension delta =
-                new Dimension(((viewDimension.width - topologyDimension.width) / 2) - topologyCoordinates.x,
-                        ((viewDimension.height - topologyDimension.height) / 2) - topologyCoordinates.y);
+        final Dimension delta = new Dimension(
+                ((viewDimension.width - topologyDimension.width) / 2) - topologyCoordinates.x,
+                ((viewDimension.height - topologyDimension.height) / 2) - topologyCoordinates.y);
         // Convert the delta to child coordinate system
         displayer.getScalableLayers().translateFromParent(delta);
 
@@ -92,6 +136,10 @@ public abstract class FitToScreenController {
                 compView.setLocation(childBoundsCoordinates);
             }
         }
+    }
+
+    public void addListener(final Listener listener) {
+        listeners.add(listener);
     }
 
 }
