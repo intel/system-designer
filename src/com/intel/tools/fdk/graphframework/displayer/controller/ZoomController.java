3fc322452f69e68c1c7f8d590b14eeba5a5f5c11
Benoit Goudy <benoit.goudy@intel.com>
2016-05-02 13:16:28 +0200

    Fix zoom controler to zoom on mouse location when zooming with scroll wheel
    and zooms on center when zooming with shortcut.
    
    Change-Id: I2c09cc3d0a16955dfbbec168f88734d63bc6d307
    Tracked-On: RTC-182814
    Signed-off-by: Benoit Goudy <benoit.goudy@intel.com>

--- src/com/intel/tools/fdk/graphframework/displayer/controller/ZoomController.java
+++ src/com/intel/tools/fdk/graphframework/displayer/controller/ZoomController.java
@@ -22,6 +22,8 @@
  */
 package com.intel.tools.fdk.graphframework.displayer.controller;
 
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.PrecisionPoint;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
@@ -46,7 +48,7 @@ public class ZoomController {
             public void mouseScrolled(final MouseEvent e) {
                 // CTRL + mouse wheel -> zoom
                 if ((e.stateMask & SWT.CTRL) == SWT.CTRL) {
-                    zoom(e.count > 0 ? zoomStep : 1 / zoomStep);
+                    zoomAt(e.count > 0 ? zoomStep : 1 / zoomStep, e.x, e.y);
                 }
             }
         });
@@ -66,17 +68,66 @@ public class ZoomController {
         });
     }
 
-    public void setFitToScreenController(final FitToScreenController fitToScreenController) {
-        this.fitToScreenController = fitToScreenController;
-    }
+    /**
+     * Zoom the specified factor to a specific point.
+     *
+     * @param factor
+     *            Zoom factor.
+     * @param x
+     *            X position on the viewport.
+     * @param y
+     *            Y position on the viewport.
+     */
+    protected void zoomAt(final double factor, final double x, final double y) {
 
-    private void zoom(final double factor) {
         // This is a manual zoom, we must disable fit to screen if we have one
         if (fitToScreenController != null) {
             fitToScreenController.setFitEnabled(false);
         }
 
+        final Point location = toScaledLayerPosition(x, y);
         displayer.setScale(displayer.getScale() * factor);
+        final Point locationAfterZoom = toScaledLayerPosition(x, y);
+        translate(new Point(location.x - locationAfterZoom.x, location.y - locationAfterZoom.y));
+    }
+
+    public void setFitToScreenController(final FitToScreenController fitToScreenController) {
+        this.fitToScreenController = fitToScreenController;
+    }
+
+    /**
+     * Convert location on the viewport to an absolute location.
+     *
+     * @param event
+     *            the received drop event
+     * @return the drop location relative to the canvas
+     */
+    private Point toScaledLayerPosition(final double x, final double y) {
+        final Point location = new PrecisionPoint(x, y);
+        displayer.getContentLayer().translateToRelative(location);
+        return location;
+    }
+
+    /**
+     * Return the center position of the visible area.
+     *
+     * @return The point corresponding to the center.
+     */
+    private Point getCenterPosition() {
+        return displayer.getContentLayer().getClientArea().getCenter();
+    }
+
+    private void translate(final Point point) {
+        final Point currentLocation = displayer.getControl().getViewport().getViewLocation();
+        final int xmove = (int) (point.x * displayer.getScale());
+        final int ymove = (int) (point.y * displayer.getScale());
+        displayer.getControl().getViewport().setViewLocation(currentLocation.x + xmove,
+                currentLocation.y + ymove);
+
+    }
+
+    private void zoom(final double factor) {
+        zoomAt(factor, getCenterPosition().x(), getCenterPosition().y());
     }
 
 }
