f24c0cd7ad93938914dc46c32d8238f9132df222
David Wagner <david.wagner@intel.com>
2017-01-09 17:53:46 +0100

    Change the license to Eclipse Public License v1.0
    Signed-off-by: David Wagner <david.wagner@intel.com>

--- java/com.intel.tools.fdk.graphframework/src/com/intel/tools/fdk/graphframework/displayer/controller/ReparentingController.java
+++ java/com.intel.tools.fdk.graphframework/src/com/intel/tools/fdk/graphframework/displayer/controller/ReparentingController.java
@@ -1,24 +1,11 @@
-/* ============================================================================
- * INTEL CONFIDENTIAL
+/*
+ * Copyright (C) 2013-2017 Intel Corporation
  *
- * Copyright 2016 Intel Corporation All Rights Reserved.
+ * This Program is subject to the terms of the Eclipse Public License, v. 1.0.
+ * If a copy of the license was not distributed with this file,
+ * you can obtain one at <http://www.eclipse.org/legal/epl-v10.html>
  *
- * The source code contained or described herein and all documents related to
- * the source code ("Material") are owned by Intel Corporation or its suppliers
- * or licensors. Title to the Material remains with Intel Corporation or its
- * suppliers and licensors. The Material contains trade secrets and proprietary
- * and confidential information of Intel or its suppliers and licensors. The
- * Material is protected by worldwide copyright and trade secret laws and
- * treaty provisions. No part of the Material may be used, copied, reproduced,
- * modified, published, uploaded, posted, transmitted, distributed, or
- * disclosed in any way without Intel's prior express written permission.
- *
- * No license under any patent, copyright, trade secret or other intellectual
- * property right is granted to or conferred upon you by disclosure or delivery
- * of the Materials, either expressly, by implication, inducement, estoppel or
- * otherwise. Any license under such intellectual property rights must be
- * express and approved by Intel in writing.
- * ============================================================================
+ * SPDX-License-Identifier: EPL-1.0
  */
 package com.intel.tools.fdk.graphframework.displayer.controller;
 
